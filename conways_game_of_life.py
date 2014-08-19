aaf4ed5e12956cfacd4d2ebf73394faab81b193c
logan <logan@Latitude.(none)>
2014-08-19 23:12:12 +0000

    entity class and position class added to the mix

--- conways_game_of_life.py
+++ conways_game_of_life.py
@@ -21,26 +21,40 @@ def main():
 	
 
 def render(universe):
-	for _x in xrange(universe.x):
+	for x in xrange(universe.x):
 		#print('_x: ' + str(_x))
 		#print('universe.x: ' + str(universe.x))
-		for _y in xrange(universe.y):
+		for y in xrange(universe.y):
 			#print('_y: ' + str(_y))
 			#print('universe.y: ' + str(universe.y))
-			print(universe.world[_x][_y]),
-			if _y == universe.y-1:
+			print(universe.world[x][y]),
+			if y == universe.y-1:
 				print('')
 			
 
 def update():
 	pass
 
+
+def get_random(lower_bound, upper_bound):
+	return random.randint(lower_bound, upper_bound)
+
+
+def print_character(universe, x, y):
+	for entity in universe.entities:
+		if entity.x == x:
+			if entity.y == y:
+				print('x')
+
+
+
 class Universe:
 	def __init__(self, x, y):
 		print('creating universe')
 		self.x = x
 		self.y = y
-		self.end_of_world = x - 1
+		#self.end_of_world = x - 1
+		self.entities = self.create_entities()
 
 		self.world = [['_' for i in xrange(y)] for j in xrange(x)]
 
@@ -52,6 +66,30 @@ class Universe:
 				grid[row].append(0)
 		#for row in xrange(int(x)): world += [[0]*int(y)]
 		return world
+
+	def create_entities(self):
+		entities = []
+		for x in xrange(self.x):
+			for y in xrange(self.y):
+				rand = get_random(0,9)
+				print('rand: ' + str(rand))
+				if rand < 3:
+					entity = Entity(x, y)
+					print('creating entity at ' + str(x) + ',' + str(y))
+					entities.append(entity)
+
+		return entities
+
+class Position:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		self.contains_entity = False
+
+class Entity:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
 
 
 if __name__ == '__main__':
